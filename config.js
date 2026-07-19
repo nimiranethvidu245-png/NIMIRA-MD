4266e0ad6be46fe639d7ba5914f47c4794178e80
Nimira Nethvidu <nimiranethvidu245@gmail.com>
2026-07-19 22:09:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/nimiranethvidu245-png/NIMIRA-MD/blob/main/Images/IMG_20260719_092754.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 NIMIRA-MD Is Alive Now😍*",
+BOT_OWNER: '94769850638',  // Replace with the owner's phone number
 
 
 
