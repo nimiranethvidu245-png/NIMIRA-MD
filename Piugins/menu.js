dbaf63157ee9953a90c8d69a068670df53a61d8f
Nimira Nethvidu <nimiranethvidu245@gmail.com>
2026-07-19 22:23:29 +0530

    Update menu.js

--- Piugins/menu.js
+++ Piugins/menu.js
@@ -8,7 +8,7 @@ cmd(
     filename: __filename,
   },
   async (
-    danuwa,
+    NIMIRA MD,
     mek,
     m,
     {
@@ -29,10 +29,66 @@ cmd(
         });
       }
 
-      let menuText = "📋 *Available Commands:*\n";
+
+
+> *·:·.┏━⋅━⋅━━⋅༻❁༺⋅━━⋅━⋅━┓:·.
+.༻♡  📥 Download Menu 📥  ♡༺.
+·:·. ╰━⋅━⋅━.· ✿༻༺✿·.━⋅━⋅━╯  :·.
+
+══════ 🎀 `.song` ══════
+   ✧ Download and play audio/mp3 files  
+   🎵 Usage: .song baby baby  
+
+══════ 🎀 `.video` ══════
+   ✧ Download high quality video files  
+   🎥 Usage: .video lelena  
+
+══════ 🎀 `.tt` ══════
+   ✧ Download TikTok videos without watermark  
+   📱 Usage: .tt <Url>  
+
+══════ 🎀 `.fb` ══════
+   ✧ Download Facebook videos and reels  
+   👥 Usage: .fb <Url>  
+
+══════ 🎀 `.apk` ══════
+   ✧ Search and download Android APK files  
+   ⚙️ Usage: .apk whatsapp  
+
+══════ 🎀 `.img` ══════
+   ✧ Search and download HD images  
+   🖼️ Usage: .img cat  
+
+══════ 🎀 `.xnxx` ══════
+   ✧ Download adult content from XNXX  
+   🔞 Usage: .xnxx mia  
+
+══════ 🎀 `.img2` ══════
+   ✧ Search and download High Quality images  
+   🖼️ Usage: .img2 Dog  
+
+══════ 🎀 `.paper` ══════
+   ✧ Download school/exam papers  
+   📝 Education Hub  
+
+══════ 🎀 `.novels` ══════
+   ✧ Download novels and story books  
+   📚 Usage: .novels Love  
+
+══════ 🎀 `.alldl` ══════
+   ✧ Download media from any link  
+   🔗 Usage: .alldl <Link>  
+
+══════ 🎀 `.mediafire` ══════
+   ✧ Direct Mediafire link downloader  
+   ☁️ Usage: .mediafire <Url>  
+
+══════ 🎀 `.stdl` ══════
+   ✧ Search and download WhatsApp stickers  
+   🎭 Usage: .stdl cat*\n";
 
       for (const [cat, cmds] of Object.entries(categories)) {
-        menuText += `\n📂 *${cat.toUpperCase()}*\n`;
+        menuText += `\n📂*${cat.toUpperCase()}*\n`;
         cmds.forEach(c => {
           menuText += `- .${c.pattern} : ${c.desc}\n`;
         });
