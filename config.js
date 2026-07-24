e0529661a129d9ffd1b83152a4e8ad0f53d2a246
Nimira Nethvidu <nimiranethvidu245@gmail.com>
2026-07-24 08:46:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,14 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "4ItRCBxQ#JgEjsMrIZft9oeXigckDIrklKmiJOHGDMKOdpZ_liZs",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/nimiranethvidu245-png/NIMIRA-MD/blob/main/Images/IMG_20260719_092754.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 NIMIRA-MD Is Alive Now😍*",
+ALIVE_MSG: process.env.ALIVE_MSG || "*『 ⚡ ᴘᴏᴡᴇʀꜰᴜʟ ᴡᴀ ⚡ 』
+      
+*👋 Hey I'am  NIMIRA MD 𝙒ᴀ ʙᴏᴛ😾, ᴀ ᴘᴏᴡᴇʀꜰᴜʟ ᴡᴀ  ᴀᴜᴛᴏᴍᴀᴛɪᴏɴ ʙᴏᴛ ᴅᴇꜱɪɢɴᴇᴅ ᴛᴏ ʜᴇʟᴘ ʏᴏᴜ ᴡɪᴛʜ ꜰᴀꜱᴛ ᴀɴᴅ ꜱᴍᴀʀᴛ ʀᴇᴘᴏɴꜱ 🫦 , ɪ ᴡᴀꜱ ᴄʀᴇᴀᴛᴇᴅ & ᴅᴇꜱɪɢɴ ニミラ・ネスヴィド🗣️🧚‍♀️*
+
+
+
+> Gʜᴏꜱᴛ ✘ ᴀɪʀ Bᴏᴛ ᴠ1.0.0
+> 🐥_Web Site: Nimira.site_*",
 BOT_OWNER: '94769850638',  // Replace with the owner's phone number
 
 
